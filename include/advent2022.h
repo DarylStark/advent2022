32d300dde1d19c3657dba481bdc682ecb46a3eb8
Daryl Stark <daryl@dstark.nl>
2022-10-29 15:31:09 +0200

    Started with the methods to set the mood lighting

--- include/advent2022.h
+++ include/advent2022.h
@@ -3,13 +3,21 @@
 
 // STL includes
 #include <string>
+#include <sstream>
+#include <functional>
+#include <vector>
 
 // Nemo Includes
 #include <nemoapp.h>
 #include <liquidcrystal.h>
 #include <binarysensor.h>
 
-#include <iostream>
+enum AppMode
+{
+    Setup = -1,
+    MoodLighting = 0,
+    Calendar = 1
+};
 
 class Advent2022 : public nemo::NemoApp
 {
@@ -21,10 +29,21 @@ private:
     nemo::BinarySensor __next;
     nemo::BinarySensor __previous;
 
+    // Objects foor the 'moodlighting'
+    std::vector<std::string> __moodlightings;
+
 public:
     Advent2022();
     void setup() override;
     void loop() override;
+
+    // Methods to switch between modes
+    void configure_mode();
+    void set_mood();
+
+    // Methods for the 'moodlighting' mode
+    void next_mood(const nemo::EntityEvent &e);
+    void previous_mood(const nemo::EntityEvent &e);
 };
 
 #endif // __ADVENT2022_H__
--- src/advent2022.cpp
+++ src/advent2022.cpp
@@ -15,32 +15,29 @@ void Advent2022::setup()
 
     // Set the welcome text
     NemoApp::entities["lcd.display.line0"] = std::string(" Adventkalender");
-    NemoApp::entities["lcd.display.line1"] = std::string("      2022");
+    NemoApp::entities["lcd.display.line1"] = std::string("Aan het starten");
 
-    // Subscribe to button entities
-    NemoApp::entities["next.sensor.low"].subscribe(
-        "next_pressed",
-        {[](const nemo::EntityEvent &e)
-         {
-             if (e.entity)
-             {
-                 NemoApp::entities["lcd.display.line1"] = std::string("      NEXT >");
-                 return;
-             }
-             NemoApp::entities["lcd.display.line1"] = std::string("      2022");
-         }});
+    // Create entities for the application
+    NemoApp::entities["advent.mode"] = AppMode::Setup;
+    NemoApp::entities["advent.mood_current"] = -1;
 
-    NemoApp::entities["previous.sensor.low"].subscribe(
-        "previous_pressed",
-        {[](const nemo::EntityEvent &e)
-         {
-             if (e.entity)
-             {
-                 NemoApp::entities["lcd.display.line1"] = std::string("    < PREV");
-                 return;
-             }
-             NemoApp::entities["lcd.display.line1"] = std::string("      2022");
-         }});
+    // Create the moods
+    __moodlightings.reserve(3);
+    __moodlightings.push_back("Off");
+    __moodlightings.push_back("Outline");
+    __moodlightings.push_back("Full");
+
+    // Add listeners to the mode-entities
+    NemoApp::entities["advent.mode"].subscribe(
+        "update_mood",
+        {std::bind(&Advent2022::configure_mode, this)});
+    NemoApp::entities["advent.mood_current"].subscribe(
+        "update_mood_current",
+        {std::bind(&Advent2022::set_mood, this)});
+
+    // Set the startmode 'moodlighting'
+    NemoApp::entities["advent.mode"] = AppMode::MoodLighting;
+    NemoApp::entities["advent.mood_current"] = 0;
 }
 
 void Advent2022::loop()
@@ -51,4 +48,72 @@ void Advent2022::loop()
 
     // Delay :)
     delay(100);
+}
+
+void Advent2022::configure_mode()
+{
+    const nemo::Entity &mode = NemoApp::entities["advent.mode"];
+
+    if (static_cast<int>(mode) == AppMode::MoodLighting)
+    {
+        // TODO: unsubscribe from the events of the calendar mode
+        NemoApp::entities["next.sensor.low"].subscribe("button_next", {std::bind(&Advent2022::next_mood, this, std::placeholders::_1)});
+        NemoApp::entities["previous.sensor.low"].subscribe("button_prev", {std::bind(&Advent2022::previous_mood, this, std::placeholders::_1)});
+
+        NemoApp::entities["lcd.display.line0"] = std::string("Sfeerverlichting");
+        NemoApp::entities["lcd.display.line1"] = std::string("");
+        return;
+    }
+
+    if (static_cast<int>(mode) == AppMode::Calendar)
+    {
+        NemoApp::entities["lcd.display.line0"] = std::string("");
+        NemoApp::entities["lcd.display.line1"] = std::string("");
+        return;
+    }
+}
+
+void Advent2022::set_mood()
+{
+    nemo::Entity &mood_current = NemoApp::entities["advent.mood_current"];
+
+    // Sanity checks
+    if (static_cast<int>(mood_current) < 0)
+    {
+        mood_current = static_cast<int>(__moodlightings.size() - 1);
+        return;
+    }
+
+    if (static_cast<int>(mood_current) >= __moodlightings.size())
+    {
+        mood_current = 0;
+        return;
+    }
+
+    // Create the string for on the screen
+    const std::string &name = __moodlightings[static_cast<int>(mood_current)];
+    const uint16_t text_space = 12;
+    uint16_t spaces_before = 6 - ((name.length() + 1) / 2);
+    uint16_t spaces_after = text_space - name.length() - spaces_before;
+
+    // Display the name on the LCD
+    std::stringstream lcd_text;
+    lcd_text << "< ";
+    lcd_text << std::string(spaces_before, ' ');
+    lcd_text << name;
+    lcd_text << std::string(spaces_after, ' ');
+    lcd_text << " >";
+    NemoApp::entities["lcd.display.line1"] = lcd_text.str();
+}
+
+void Advent2022::next_mood(const nemo::EntityEvent &e)
+{
+    if (e.entity)
+        NemoApp::entities["advent.mood_current"]++;
+}
+
+void Advent2022::previous_mood(const nemo::EntityEvent &e)
+{
+    if (e.entity)
+        NemoApp::entities["advent.mood_current"]--;
 }
